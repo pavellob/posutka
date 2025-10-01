408884fbd4b0ccbef9b45a66f0bd69b90c626527
pavel lobachev <pavellobachev@pavels-MacBook-Air.local>
2025-10-01 15:10:45 +0300

    grpc

--- packages/grpc-sdk/src/index.ts
+++ packages/grpc-sdk/src/index.ts
@@ -1,6 +1,6 @@
 // Экспорт клиентов
 export * from './clients/ops.client.js';
 
-// Экспорт сгенерированных типов
-export * from './generated/ops.js';
+// Типы экспортируются из клиента
+// export * from './generated/ops.js';  // Закомментировано - вызывает проблемы при билде
 
